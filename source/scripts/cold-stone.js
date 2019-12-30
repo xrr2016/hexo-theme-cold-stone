c0994af3f9a98108f6cdc71dc9efdd825932da9d
Cold Stone <xiaoranran1993@outlook.com>
2019-12-30 12:47:30 +0800

    feat: style update

--- source/scripts/cold-stone.js
+++ source/scripts/cold-stone.js
@@ -1,4 +1,33 @@
+const themes = {
+  dark: 'dark',
+  light: 'light'
+}
+
+const documentHead = document.getElementsByTagName('head')[0]
+
+const localTheme = localStorage.getItem('theme')
 const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+const darkThemeHref = 'https://cdn.bootcss.com/highlight.js/9.15.10/styles/atom-one-dark.min.css'
+
+function insertThemeDarkLink() {
+  const link = document.createElement('link')
+  link.rel = 'stylesheet'
+  link.href = darkThemeHref
+
+  documentHead.appendChild(link)
+}
+
+function removeThemeDarkLink() {
+  const links = document.querySelectorAll('link')
+
+  for (let i = 0; i < links.length; i++) {
+    const link = links[i]
+
+    if (link.href === darkThemeHref) {
+      documentHead.removeChild(link)
+    }
+  }
+}
 
 // service worker
 if ('serviceWorker' in navigator) {
@@ -23,9 +52,9 @@ const navs = [
 if (path === '/' || /page/.test(path)) {
   navLinkList[0].classList.add('active')
 } else {
-  navs.forEach(function(nav, index) {
+  navs.forEach(function (nav, index) {
     if (nav === path) {
-      const item = navLinkList.find(function(item) {
+      const item = navLinkList.find(function (item) {
         const link = item.dataset.link
         return link === nav
       })
@@ -40,7 +69,7 @@ if (path === '/' || /page/.test(path)) {
 // article toc
 const tocLinkList = document.querySelectorAll('.toc-link')
 
-tocLinkList.forEach(function(link) {
+tocLinkList.forEach(function (link) {
   const href = link.href
   const newHref = href.replace(location.origin + '/', location.href)
   link.setAttribute('href', newHref)
@@ -58,15 +87,18 @@ if (utteranc) {
   uscript.src = 'https://utteranc.es/client.js'
   uscript.setAttribute('repo', window.COLD_STONE.repo)
   uscript.setAttribute('issue-term', 'pathname')
-  if (isDarkMode) {
+  const isDarkTheme = isDarkMode || localTheme === themes.dark
+  if (isDarkTheme) {
+    insertThemeDarkLink()
     uscript.setAttribute('theme', 'github-dark')
   } else {
     uscript.setAttribute('theme', 'github-light')
+    removeThemeDarkLink()
   }
   utteranc.appendChild(uscript)
 
   // 假装在加载
-  const timeout = setTimeout(function() {
+  const timeout = setTimeout(function () {
     loader.remove()
     clearTimeout(timeout)
   }, 3000)
@@ -76,7 +108,7 @@ if (utteranc) {
 const back = document.getElementById('back')
 
 if (back) {
-  back.addEventListener('click', function() {
+  back.addEventListener('click', function () {
     history.back()
   })
 }
@@ -95,7 +127,7 @@ if (backTop) {
     }
   })
 
-  backTop.addEventListener('click', function() {
+  backTop.addEventListener('click', function () {
     window.scroll({
       top: 0,
       left: 0,
@@ -103,3 +135,85 @@ if (backTop) {
     })
   })
 }
+
+// toggle theme
+const toggle = document.querySelector('.footer-toggle')
+
+const darkBtn = document.querySelector('.dark-btn')
+const lightBtn = document.querySelector('.light-btn')
+
+
+
+if (localTheme === themes.dark) {
+  document.body.classList.add('dark')
+  insertThemeDarkLink()
+  if (darkBtn) {
+    darkBtn.classList.remove('show')
+    lightBtn.classList.add('show')
+  }
+} else {
+  removeThemeDarkLink()
+  if (darkBtn) {
+    lightBtn.classList.remove('show')
+    darkBtn.classList.add('show')
+  }
+}
+
+function findTheme(target) {
+  const theme = target.dataset.theme
+  if (theme) {
+    return theme
+  }
+  return findTheme(target.parentElement)
+}
+
+if (toggle) {
+
+  toggle.addEventListener('click', event => {
+    const theme = findTheme(event.target)
+    localStorage.setItem('theme', theme)
+
+    switch (theme) {
+      case themes.dark:
+        document.body.classList.add('dark')
+        darkBtn.classList.remove('show')
+        lightBtn.classList.add('show')
+        break;
+      case themes.light:
+        removeThemeDarkLink()
+        document.body.classList.remove('dark')
+        lightBtn.classList.remove('show')
+        darkBtn.classList.add('show')
+        break;
+    }
+  })
+}
+
+// article
+const article = document.querySelector('.article')
+
+if (article) {
+  const articleToc = document.querySelector('.article-toc')
+
+  article.addEventListener('mouseenter', () => {
+    // const timeout = setTimeout(() => {
+    //   articleToc.classList.add('fade')
+    //   clearTimeout(timeout)
+    // }, 300)
+    articleToc.classList.add('fade')
+  })
+
+  article.addEventListener('mouseleave', () => {
+    articleToc.classList.remove('fade')
+  })
+}
+
+// wave
+// const wave = document.getElementById('wave')
+
+// if (wave) {
+//   const colors = ['#ffffff']
+//   const random = colors[Math.floor(Math.random() * colors.length)]
+//   const path = wave.getElementsByTagName('path')[0]
+//   path.setAttribute('fill', random)
+// }
